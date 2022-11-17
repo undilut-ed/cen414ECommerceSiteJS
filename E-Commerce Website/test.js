d99ca302d6bed65ad7e05d45c890192af5d4fe7c
Edwin Ade <114532254+undilut-ed@users.noreply.github.com>
2022-11-17 22:36:37 +0100

    Delete E-Commerce Website directory

--- E-Commerce Website/test.js
+++
@@ -1,9 +0,0 @@
-// var myNum = 40002000.00
-
-// console.log(myNum.toLocaleString("en-US", {minimumFractionDigits: 2}));
-
-let myObj = {
-    yes: "yellow",
-    two: "no"
-}
-console.log(Object.entries(myObj));
