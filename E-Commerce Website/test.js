37b424791f84c93880659bd2616717f114cb5e5a
Edwin Ade <114532254+undilut-ed@users.noreply.github.com>
2022-11-17 22:35:11 +0100

    Uploaded init files

---
+++ E-Commerce Website/test.js
@@ -0,0 +1,9 @@
+// var myNum = 40002000.00
+
+// console.log(myNum.toLocaleString("en-US", {minimumFractionDigits: 2}));
+
+let myObj = {
+    yes: "yellow",
+    two: "no"
+}
+console.log(Object.entries(myObj));
